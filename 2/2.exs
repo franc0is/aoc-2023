845748c71d423d5b0af3e8799254a2077efc528b
Francois Baldassari <francois@memfault.com>
2023-12-02 23:34:15 -0500

    Add a few code comments

--- 2/2.exs
+++ 2/2.exs
@@ -1,15 +1,18 @@
 defmodule Part2 do
   def decode_line(line) do
     [_ | draws] = String.split(line, [";", ":"])
+    # Create array of dictionaries (one for each draw) of the form {red: n, blue: m, green: p}
     Enum.map(draws, fn draw -> 
       Regex.scan(~r/(\d+)\s*(blue|green|red)/, draw)
       |> Enum.reduce(%{}, fn [_, number, color], acc ->
         Map.put(acc, color, String.to_integer(number))
       end)
     end)
+    # Merge all dictionaries key-wise, using the max function as comparator
     |> Enum.reduce(%{}, &Map.merge(&1, &2, fn _k1, v1, v2 ->
                           max(v1, v2)
                         end))
+    # Multiplies the values together
     |> Map.values
     |> Enum.reduce(1, &(&1 * &2))
   end
@@ -36,10 +39,13 @@ defmodule Part1 do
     [_, game_id] = Regex.run(~r/Game (\d+)/, game)
     valid = Enum.map(draws, fn draw -> 
                Regex.scan(~r/(\d+)\s*(blue|green|red)/, draw)
+               # for each draw of each color, compare number of cubes against the inventory
+               # this will create an array of booleans (one for each draw)
                |> Enum.reduce(true, fn [_, number, color], acc ->
                  acc and (Map.fetch!(@inventory, color) >= String.to_integer(number))
                end)
              end)
+             # Verify that all draws in this game are valid
              |> Enum.all?
     case valid do
       true ->
