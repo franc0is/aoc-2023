db4a5bea17e1f8b88a76478436f6ffe25d56bb19
Francois Baldassari <francois@memfault.com>
2023-12-04 23:03:57 -0500

    Day 4 part 2

--- 4/4.exs
+++ 4/4.exs
@@ -24,6 +24,23 @@ defmodule Day4 do
     end)
   end
 
+  # Q: How much do I like recursion?
+  # A: Quite a lot.
+  def process_game_by_index(lut, index, acc \\ 0) do
+    n_matches = lut
+                |> Enum.at(index)
+
+    match_list = if n_matches > 0 do Enum.to_list(1..n_matches) else [] end
+    process_matches(match_list, lut, index, acc + 1)
+  end
+
+  defp process_matches([], _lut, _index, acc), do: acc
+
+  defp process_matches([match | rest], lut, index, acc) do
+    new_acc = acc + process_game_by_index(lut, index + match)
+    process_matches(rest, lut, index, new_acc)
+  end
+
   def part1(file_path) do
     File.stream!(file_path)
     |> Enum.reduce(0, fn line, score ->
@@ -36,12 +53,25 @@ defmodule Day4 do
   end
 
   def part2(file_path) do
-    :ok
+    # We cache the number of matches in a LUT
+    # Otherwise it's slow
+    lut = File.stream!(file_path)
+          |> Enum.map(fn line ->
+            line
+            |> String.replace("\n", "")
+            |> find_matches
+          end)
+
+    lut
+    |> Enum.with_index
+    |> Enum.reduce(0, fn {_, index}, acc ->
+      acc + process_game_by_index(lut, index)
+    end)
   end
 end
 
 
 part1 = Day4.part1('input.txt')
 IO.puts("Part 1: #{part1}")
-part2 = Day4.part2('test.txt')
+part2 = Day4.part2('input.txt')
 IO.puts("Part 2: #{part2}")
