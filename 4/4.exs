466757c0bd649bdac3cc63817b1c987724c3cfea
Francois Baldassari <francois@memfault.com>
2023-12-04 22:04:11 -0500

    Part 2 scaffolding

--- 4/4.exs
+++ 4/4.exs
@@ -5,7 +5,7 @@ defmodule Day4 do
     |> Enum.map(&String.to_integer(&1))
   end
 
-  def score_line(line) do
+  def find_matches(line) do
     # Discard game number
     [_, numbers] = String.split(line, ":")
 
@@ -14,17 +14,14 @@ defmodule Day4 do
       |> String.split("|")
       |> Enum.map(fn s -> string_to_numbers(s) end)
 
-    matches = 
-      numbers_we_have
-      |> Enum.reduce(0, fn n, acc ->
-        if n in winning_numbers do
-          acc + 1 
-        else
-          acc
-        end
-      end)
-
-    trunc(:math.pow(2, matches - 1))
+    numbers_we_have
+    |> Enum.reduce(0, fn n, acc ->
+      if n in winning_numbers do
+        acc + 1 
+      else
+        acc
+      end
+    end)
   end
 
   def part1(file_path) do
@@ -32,12 +29,19 @@ defmodule Day4 do
     |> Enum.reduce(0, fn line, score ->
       line
       |> String.replace("\n", "")
-      |> score_line
-      |> (fn x -> x + score end).() # because can't pipe into +
+      |> find_matches
+      |> then(fn m -> trunc(:math.pow(2, m - 1)) end) # Score game
+      |> then(fn s -> score + s end) # Accumulate score
     end)
+  end
+
+  def part2(file_path) do
+    :ok
   end
 end
 
 
 part1 = Day4.part1('input.txt')
 IO.puts("Part 1: #{part1}")
+part2 = Day4.part2('test.txt')
+IO.puts("Part 2: #{part2}")
