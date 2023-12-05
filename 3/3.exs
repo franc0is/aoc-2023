c6002f4e4b7946ed0ed0a03fbf986c3b03ec5e25
Francois Baldassari <francois@memfault.com>
2023-12-04 21:51:51 -0500

    Day 3 part 2, ugly edition

--- 3/3.exs
+++ 3/3.exs
@@ -67,6 +67,43 @@ defmodule Day3 do
     n
   end
 
+  def find_and_multiply_gears({numbers, symbols}) do
+    {n, _} = symbols
+    |> Enum.filter(fn s -> s.value == "*" end)
+    |> Enum.reduce({0, numbers}, fn s, {acc, numbers} ->
+      #IO.inspect(s)
+      potential_gears = numbers
+                        |> Enum.reduce([], fn n, parts ->
+                          if Token.adjacent?(s, n) do
+                            [n | parts]
+                          else
+                            parts
+                          end
+                        end)
+      if length(potential_gears) == 2 do
+        {acc + Enum.at(potential_gears, 0).value * Enum.at(potential_gears, 1).value, numbers}
+      else
+        {acc, numbers}
+      end
+    end)
+    n
+  end
+
+  def part2(file_path) do
+    file_stream = File.stream!(file_path)
+    columns = (file_stream |> Enum.take(1) |> List.first |> String.length) - 1
+
+    file_stream
+    |> Enum.with_index
+    |> Enum.reduce({[], []}, fn {line, y}, {numbers, symbols} ->
+      line = line
+             |> String.replace("\n", "")
+      {n, s} = parse_line(line, y, columns)
+      {numbers ++ n, symbols ++ s}
+    end)
+    |> find_and_multiply_gears
+  end
+
   def part1(file_path) do
     file_stream = File.stream!(file_path)
     columns = (file_stream |> Enum.take(1) |> List.first |> String.length) - 1
@@ -86,3 +123,6 @@ end
 
 part1 = Day3.part1('input.txt')
 IO.puts("Part 1: #{part1}")
+part2 = Day3.part2('input.txt')
+IO.puts("Part 2: #{part2}")
+
